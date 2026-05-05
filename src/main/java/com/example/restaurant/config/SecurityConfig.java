18cfd8fd5754ea6ff72a39be74160435f0c4dc8c
mateusz-bogacz-collegiumwitelona <mateusz.bogaczdrewniak@proton.me>
2026-05-05 23:38:29 +0200

    secure config

--- src/main/java/com/example/restaurant/config/SecurityConfig.java
+++ src/main/java/com/example/restaurant/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-
+        csrfTokenRepository.setCookieCustomizer(cookie -> cookie.domain(".quilacarne.com.pl"));
+        
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName(null);
 
