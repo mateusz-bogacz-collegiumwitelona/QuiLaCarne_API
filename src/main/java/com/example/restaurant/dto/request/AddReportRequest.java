6d2151875f71dd80406295b1a89f8b5a7e2988a3
mateusz-bogacz-collegiumwitelona <mateusz.bogaczdrewniak@proton.me>
2026-06-02 16:14:01 +0200

    more samples, change name

--- src/main/java/com/example/restaurant/dto/request/AddReportRequest.java
+++ src/main/java/com/example/restaurant/dto/request/AddReportRequest.java
@@ -10,6 +10,6 @@ public class AddReportRequest {
   private String clientToken;
 
   @NotBlank(message = "Reason cannot be empty")
-  @Size(min = 10, max = 500, message = "Reason must be between 10 and 500 characters")
+  @Size(min                   = 10, max = 500, message = "Reason must be between 10 and 500 characters")
   private String reason;
 }
